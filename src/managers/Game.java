637dedf7bd8fe75f8d2f5e1f02891a84cb10dac1
evannudd <evan.nudd@mymail.champlain.edu>
2018-04-24 11:38:58 -0400

    updated gitignore for Eclipse, disabled GL3

--- src/managers/Game.java
+++ src/managers/Game.java
@@ -7,43 +7,43 @@ import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class Game implements ApplicationListener {
-    public static int WIDTH;
-    public static int HEIGHT;
+	public static int WIDTH;
+	public static int HEIGHT;
 
-    private PlayState ps;
+	private PlayState ps;
 
-    public void create() {
-        WIDTH = Gdx.graphics.getWidth();
-        HEIGHT = Gdx.graphics.getHeight();
+	public void create() {
+		WIDTH = Gdx.graphics.getWidth();
+		HEIGHT = Gdx.graphics.getHeight();
 
-        OrthographicCamera cam = new OrthographicCamera(WIDTH, HEIGHT);
-        cam.translate(WIDTH / 2, HEIGHT / 2);
-        cam.update();
+		OrthographicCamera cam = new OrthographicCamera(WIDTH, HEIGHT);
+		cam.translate(WIDTH / 2, HEIGHT / 2);
+		cam.update();
 
-        Gdx.input.setInputProcessor(new InputAdapter());
+		Gdx.input.setInputProcessor(new InputAdapter());
 
-        ps = new PlayState();
-    }
+		ps = new PlayState();
+	}
 
-    public void render() {
-        // clear screen to black
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+	public void render() {
+		// clear screen to black
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 
-        ps.update(Gdx.graphics.getDeltaTime() * Settings.TIME_MULTIPLIER);
-        ps.draw();
-    }
+		ps.update(Gdx.graphics.getDeltaTime() * Settings.TIME_MULTIPLIER);
+		ps.draw();
+	}
 
-    public void resize(int width, int height) {
-    }
+	public void resize(int width, int height) {
+	}
 
-    public void pause() {
-    }
+	public void pause() {
+	}
 
-    public void resume() {
-    }
+	public void resume() {
+	}
 
-    public void dispose() {
-    }
+	public void dispose() {
+	}
 
 }
--- src/managers/Main.java
+++ src/managers/Main.java
@@ -4,14 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
-    public static void main(String[] args) {
-        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "Evolution";
-        cfg.width = Settings.WINDOW_WIDTH;
-        cfg.height = Settings.WINDOW_HEIGHT;
-        cfg.useGL30 = true;
-        cfg.resizable = false;
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Evolution";
+		cfg.width = Settings.WINDOW_WIDTH;
+		cfg.height = Settings.WINDOW_HEIGHT;
+		cfg.useGL30 = false;
+		cfg.resizable = false;
 
-        new LwjglApplication(new Game(), cfg);
-    }
+		new LwjglApplication(new Game(), cfg);
+	}
 }
